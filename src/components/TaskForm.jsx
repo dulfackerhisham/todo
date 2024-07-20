50834e6842ed7b2760162ba7d81e29f97e6163db
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-07-20 11:32:36 +0530

    multiple state handled together

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,27 +3,47 @@ import Tag from "./Tag";
 import { useState } from "react";
 
 const TaskForm = () => {
-    const [task, setTask] = useState("");
 
-    const [status, setStatus] = useState("todo")
+    const [taskData, setTaskData] = useState({
+        task: "",
+        status: "todo"
+    })
 
-    const handleSetTask = (e) => {
-        setTask(e.target.value)
+    const handleChange = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+
+        setTaskData((prev) => {
+            // console.log(prev, "prev state");
+            return {...prev, [name]: value}
+        })
+
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(taskData);
     }
+    // const [task, setTask] = useState("");
 
-    const handleSetStatus = (e) => {
-        setStatus(e.target.value)
-    }
+    // const [status, setStatus] = useState("todo")
 
-    console.log(task);
-    console.log(status);
+    // const handleSetTask = (e) => {
+    //     console.log(e.target.value, "e.target");
+    //     setTask(e.target.value)
+    // }
+
+    // const handleSetStatus = (e) => {
+    //     setStatus(e.target.value)
+    // }
 
     return(
         <header className="header">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" className="task-input"
+                name="task"
                 placeholder="Enter Task"
-                onChange={handleSetTask} />
+                onChange={handleChange} />
 
                 <div className="form-bottom">
                     <div>
@@ -35,7 +55,7 @@ const TaskForm = () => {
 
 
                     <div>
-                    <select className="task-status" onChange={handleSetStatus}>
+                    <select className="task-status" onChange={handleChange} name="status">
                         <option value="todo">To do</option>
                         <option value="doing">Doing</option>
                         <option value="done">Done</option>
